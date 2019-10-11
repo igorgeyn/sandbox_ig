9bdd23b40eb971b7b1e98ac8b17e68207638782e
igorgeyn <igorgeyn@gmail.com>
2019-10-10 23:39:16 -0400

    Finally making updates to the playing with python file

--- playing with python.py
+++ playing with python.py
@@ -13,4 +13,21 @@ test_list
 
 for number in test_list
     # print (div2_add20(number))
-    print
+    print("I am making some modifications to",number)
+
+# This was me playing a bit with nestd for-loops, if statemnts, etc. It worked!
+for number in test_list:
+    # print(div2_add20(number))
+    if number == 0:
+        print("This is a zero, dummy. What did you think was gonna happen?")
+    else:
+        print(number)
+        if number % 2 == 0:
+            print("I am making some modifications to a number I dare you to do something about it! By the way, this number is even.")
+        #print("I am making some modifications to",number,"and I dare you to do something about it! By the way, this number is even.")
+        elif number % 3 == 0:
+            print("I am making some modifications to a number I dare you to do something about it! By the way, this number is not even, but it is divisible by three.")
+        #print("I am making some modifications to",number,"and I dare you to do something about it! By the way, this number is not even, but is divisible by three.")
+        else: 
+            print("I am making some modifications to a number that is neither even nor divisible by three.")
+        #print("I am making some modifications to"number,"which is neither even nor divisible by three.")
