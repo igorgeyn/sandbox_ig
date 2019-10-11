8c66164521196f040960bae6976b33c5e1a68d4e
Igor Geyn <igorgeyn@gmail.com>
2019-10-10 23:06:28 -0400

    Adding python script to play with

---
+++ playing with python.py
@@ -0,0 +1,16 @@
+letters_list = ['a', 'b', 'c', 'd']
+for letter in letters_list:
+    print letter
+
+a = 8*7
+
+div2_test values = [1, 58, 9928, 464637]
+for number in div2_test_valus:
+    print(div2_add20(number))
+
+test_list = list(range(15))
+test_list
+
+for number in test_list
+    # print (div2_add20(number))
+    print
